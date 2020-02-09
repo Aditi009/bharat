0a85ba7323673ac5b655c2c094e1dd159ad3939a
Aditi009 <55443683+Aditi009@users.noreply.github.com>
2020-02-09 11:35:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,355 @@
+<html>
+	<head>
+		<link href='https://fonts.googleapis.com/css?family=Eagle Lake' rel='stylesheet'>
+		<link rel="stylesheet" type="text/css" href="login3.css" />
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+		<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	</head>
+	<body>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light" >
+  <a class="navbar-brand" href="#" style="color: crimson" >Bharatvisit.com</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown"
+	   
+	   >
+    <ul class="navbar-nav" style="margin-left: 56%">
+      <li class="nav-item active">
+        <a class="nav-link" href="#" style="box-shadow:5px 5px 5px crimson;height:50px;width:100px;border-radius:20px;text-align:center;">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#" style="box-shadow:5px 5px 5px crimson;height:50px;width:100px;border-radius:20px;text-align:center;" onclick="abc()"  
+		   >Login</a>
+      </li>
+      <li class="nav-item" onclick="alert()">
+        <a class="nav-link" href="#" style="box-shadow:5px 5px 5px crimson;height:50px;width:100px;border-radius:20px;text-align:center;">Visit</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="box-shadow:5px 5px 5px crimson;height:50px;width:135px;border-radius:20px;text-align:center;">
+          sigh in
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="border:2px solid crimson">
+          <a class="dropdown-item" href="#">Facebook</a>
+          <a class="dropdown-item" href="#">Instagram</a>
+          <a class="dropdown-item" href="#">Gmail</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+		<div id="svg" style="height:10%">
+			</div>	
+			
+			<h1 align="center"
+				><span>B</span>HARAT <span>V</span>
+			ISIT</h1>
+		
+		
+		
+		
+		</header>
+		<article>
+		<div id="img_slid">	<div class="front">
+				<form action="dblogin.php" method="post" enctype="multipart/form-data">
+				
+				<center>		
+				<span><p id="em">EMAIL</p></span>
+				<input type="email" name="roll_no" required>
+					<br>
+				<br>
+				<span><p id="em">PASSWORD</p></span>
+				<input type="password" name="email" required >
+				
+					<br>
+					<br>
+					<br>
+								
+					<button id="but2"  
+									onmouseover="on2()" onclick="on()" 	>	
+									<h3>Select Cover Photo</h3></button>
+					
+							<br>
+							<br>
+					
+							
+		<input id="btn2" 
+			   TYPE="BUTTON" onclick="abc2()" value="CANCEL"/>
+							
+		<input id="btn2" type="submit" name="submit" onclick="abc2()" value="UPLOAD"/>		
+							
+				
+					</center>
+						
+						
+		
+					<div id="cardimg">
+						<img id="img" src="user.png">
+						<input type="file" name="img1" id="inp" required="required"/>
+						<button value="save" type="submit" id="but3">Save Image</button>
+						<br><br>
+						<br>
+					<button><img id="img2" src="back.png" onclick="im()"></button>	
+				</div>
+					</form>
+				
+			</div></div>
+			
+		
+		
+		</article>
+	<center>
+		<div id="sec2"><p><h1>I</h1>ndia (Hindi: Bhārat), officially the Republic of India (Hindi: Bhārat Gaṇarājya),[20] is a country in South Asia. It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world. Bounded by the Indian Ocean on the south, the Arabian Sea on the southwest, and the Bay of Bengal on the southeast, it shares land borders with Pakistan to the west;[e] China, Nepal, and Bhutan to the north; and Bangladesh and Myanmar to the east. In the Indian Ocean, India is in the vicinity of Sri Lanka and the Maldives; its Andaman and Nicobar Islands share a maritime border with Thailand and Indonesia.<br>
+
+Modern humans arrived on the Indian subcontinent from Africa no later than 55,000 years ago.[21] Their long occupation, initially in varying forms of isolation as hunter-gatherers, has made the region highly diverse, second only to Africa in human genetic diversity.[22] Settled life emerged on the subcontinent in the western margins of the Indus river basin 9,000 years ago, evolving gradually into the Indus Valley Civilisation of the third millennium BCE.[23] By 1200 BCE, an archaic form of Sanskrit, an Indo-European language, had diffused into India from the northwest, unfolding as the language of the Rigveda, and recording the dawning of Hinduism in India.[24] The Dravidian languages of India were supplanted in the northern regions.[25] By 400 BCE, stratification and exclusion by caste had emerged within Hinduism,[26] and Buddhism and Jainism had arisen, proclaiming social orders unlinked to heredity.[27] Early political consolidations gave rise to the loose-knit Maurya and Gupta Empires based in the Ganges Basin.<br> Their collective era was suffused with wide-ranging creativity,[29] but also marked by the declining status of women,[30] and the incorporation of untouchability into an organised system of belief.[f][31] In south India, the Middle kingdoms exported Dravidian-languages scripts and religious cultures to the kingdoms of southeast Asia.[32]</div></center>
+		<div id="cards"><div class="card" style="width: 15rem; height:400px;margin-top: 3%;float: left; background-color: crimson;" onclick="cards1()">
+  <img src="pop-zebra-2SirKdwlh5c-unsplash.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text" color="crimson" style=" color: crimson">“Caring about others, running the risk of feeling, and leaving an impact on people, brings happiness.” ― Harold Kushner</p>
+    <a href="#" class="btn btn-primary" style="background-color:crimson;box-shadow: 2px 2px 2px black	;border:2px solid pink;
+											   ">Go somewhere</a>
+  </div>
+</div>
+		<div class="card" style="width: 15rem; height:400px;margin-top: 3%;float: left; left: 2%; background-color: crimson;">
+  <img src="tom-watkins-T3EAIfDZScQ-unsplash.jpg"class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text" color="crimson" style=" color: crimson">Dipped in hues of love and trust has come the festival of Holi.Let the colours of Holi spread the message of peace and happiness.
+– unknown</p>
+    <a href="#" class="btn btn-primary" style="background-color:crimson;box-shadow: 2px 2px 2px black	;border:2px solid pink;
+											   ">Go somewhere</a>
+			</div></div>
+			<div class="card" style="width: 15rem; height:400px;margin-top: 3%;float: left; left: 5%; background-color: crimson;">
+  <img src="jose-aragones-Bg7PsenHNeE-unsplash.jpg"class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text" color="crimson" style=" color: crimson"><br>
+		<br>A saint is a person who is recognized as having an exceptional degree of holiness or likeness or closeness to God.
+– Lao Tzu
+</p>
+    <a href="#" class="btn btn-primary" style="background-color:crimson;box-shadow: 2px 2px 2px black	;border:2px solid pink;
+											   ">Go somewhere</a>
+			</div></div>
+			<div class="card" style="width: 15rem; height:400px;margin-top: 3%;float: left; left: 8%; background-color: crimson;">
+  <img src="abhas-mishra-BD3qGeorAFY-unsplash.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+	  <p class="card-text" color="crimson" style=" color: crimson">they say those who have less often give more because they know exactly how it feels to have nothing and be helpless in the face of adversity.</p>
+    <a href="#" class="btn btn-primary" style="background-color:crimson;box-shadow: 2px 2px 2px black	;border:2px solid pink;
+											   ">Go somewhere</a>
+			</div></div>
+			<div class="card" style="width: 15rem; height:400px;margin-top: 3%;float: left; left: 10%; background-color: crimson;">
+  <img src="debashis-biswas-BnmJShRjx8s-unsplash.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text" color="crimson" style=" color: crimson"> “Love is like a beautiful flower which I may not touch, but whose fragrance makes the garden a place of delight just the same.”
+		– Helen Keller</p>
+    <a href="#" class="btn btn-primary" style="background-color:crimson;box-shadow: 2px 2px 2px black	;border:2px solid pink;
+											   ">Go somewhere</a>
+			</div></div>
+		</div>
+		
+		<div id="para2">
+			<p>India is the world's most populous democracy.[201] A parliamentary republic with a multi-party system,[202] it has eight recognised national parties, including the Indian National Congress and the Bharatiya Janata Party (BJP), and more than 40 regional parties.[203] The Congress is considered centre-left in Indian political culture,[204] and the BJP right-wing.[205][206][207] For most of the period between 1950—when India first became a republic—and the late 1980s, the Congress held a majority in the parliament. Since then, however, it has increasingly shared the political stage with the BJP,[208] as well as with powerful regional parties which have often forced the creation of multi-party coalition governments at the centre.[
+</p>
+		</div>
+	<center>	<h1><span>B</span>ooking Visit Of <span>B</span>harat</h1></center>
+	<div id="container"><div class="card" style="width: 30rem; margin-left: 10%; margin-top: 5% ;
+								 box-shadow: 5px 5px 10px crimson">
+  <img src="abdullah-ahmad-F9fBJ91kiHI-unsplash.jpg"    class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Our history books always described Lucknow as the city of Nawabs, now you don’t see them anymore around, but the legacy of ‘Tehzeeb’ that they left behind is still the trademark of the city, that if you take time to mingle with the old timers of the city..</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">NAWAB</li>
+    <li class="list-group-item">Delicious food</li>
+    <li class="list-group-item">
+	  Tehzeeb</li>
+  </ul>
+  <div class="card-body" >
+    <a href="https://www.goibibo.com/flights/" class="card-link" style="color:crimson;">Flight Book</a>
+    <a href="https://www.makemytrip.com/railways/?cmp=SEM|D|Rail|G|Brand|Rail_Brand_Exact|Train|ETA|Regular|325113663526&s_kwcid=AL!1631!3!325113663526!e!!g!!make.my%20trip%20train" class="card-link" style="color:crimson;">Train book</a>
+  </div>
+</div>
+		<a href="https://en.wikipedia.org/wiki/Lucknow"><button style="margin-left: 50%;color:crimson;height: 40px;width:30%;margin-top: -30%">MORE ABOUT LUCKNOW</button></a>
+		<div class="card" style="width: 30rem; margin-left: 10%; margin-top: 5% ;
+								 box-shadow: 5px 5px 10px crimson">
+  <img src="mumbai-1370023_1920.jpg"    class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Our history books always described Lucknow as the city of Nawabs, now you don’t see them anymore around, but the legacy of ‘Tehzeeb’ that they left behind is still the trademark of the city, that if you take time to mingle with the old timers of the city..</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">NAWAB</li>
+    <li class="list-group-item">Delicious food</li>
+    <li class="list-group-item">
+	  Tehzeeb</li>
+  </ul>
+  <div class="card-body" >
+    <a href="https://www.goibibo.com/flights/" class="card-link" style="color:crimson;">Flight Book</a>
+    <a href="https://www.makemytrip.com/railways/?cmp=SEM|D|Rail|G|Brand|Rail_Brand_Exact|Train|ETA|Regular|325113663526&s_kwcid=AL!1631!3!325113663526!e!!g!!make.my%20trip%20train" class="card-link" style="color:crimson;">Train book</a>
+  </div>
+</div>
+		<a href="https://en.wikipedia.org/wiki/Mumbai"><button style="margin-left: 50%;color:crimson;height: 40px;width:30%;margin-top: -30%">MORE ABOUT MUMBAI</button></a>
+		<div class="card" style="width: 30rem; margin-left: 10%; margin-top: 5% ;
+								 box-shadow: 5px 5px 10px crimson">
+  <img src="satyajeet-mazumdar-RxYnq-q_DGc-unsplash.jpg"    class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">
+The city has allured almost everyone, no matters from which field they belong to. We have compiled a list of famous Mumbai quotes by someone renowned or someone anonymous that define the city in an amazing way. Read these beautiful quotes on Mumbai beautifully describing the city.
+
+		<a style="color:pink;" href="https://en.wikipedia.org/wiki/Narendra_Modi">Narendra Modi</a></p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">NAWAB</li>
+    <li class="list-group-item">Delicious food</li>
+    <li class="list-group-item">
+	  Tehzeeb</li>
+  </ul>
+  <div class="card-body" >
+    <a href="https://www.goibibo.com/flights/" class="card-link" style="color:crimson;">Flight Book</a>
+    <a href="https://www.makemytrip.com/railways/?cmp=SEM|D|Rail|G|Brand|Rail_Brand_Exact|Train|ETA|Regular|325113663526&s_kwcid=AL!1631!3!325113663526!e!!g!!make.my%20trip%20train" class="card-link" style="color:crimson;">Train book</a>
+  </div>
+</div>
+		<a href="https://en.wikipedia.org/wiki/Gujarat"><button style="margin-left: 50%;color:crimson;height: 40px;width:30%;margin-top: -30%">MORE ABOUT GUJRAT</button></a>
+		<div class="card" style="width: 30rem; margin-left: 10%; margin-top: 5% ;
+								 box-shadow: 5px 5px 10px crimson">
+  <img src="Lotus_Temple_at_Night.jpg"   class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">The centuries-old history and culture of India, majestic architectural monuments and museums of Delhi, Agra and Mumbai have a unique attractive force.
+Read more at https://www.brainyquote.com/topics/delhi-quotes.</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">NAWAB</li>
+    <li class="list-group-item">Delicious food</li>
+    <li class="list-group-item">
+	  Tehzeeb</li>
+  </ul>
+  <div class="card-body" >
+    <a href="https://www.goibibo.com/flights/" class="card-link" style="color:crimson;">Flight Book</a>
+    <a href="https://www.makemytrip.com/railways/?cmp=SEM|D|Rail|G|Brand|Rail_Brand_Exact|Train|ETA|Regular|325113663526&s_kwcid=AL!1631!3!325113663526!e!!g!!make.my%20trip%20train" class="card-link" style="color:crimson;">Train book</a>
+  </div>
+</div>
+		
+		<a href="https://en.wikipedia.org/wiki/Delhi">	<button style="margin-left: 50%;height: 40px;width:30%; margin-top: -30%; color:crimson;">MORE ABOUT DELHI</button></a>
+	
+		
+		
+		
+		<div class="card" style="width: 30rem; margin-left: 10%; margin-top: 5% ;
+								 box-shadow: 5px 5px 10px crimson">
+  <img src="derek-story-vl69QxcojR0-unsplash.jpg"   class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Resplendent in the hues of its noble and magnificent past, the historic city of Jaipur stands out as one of the most spectacular and culturally vibrant destinations in the world..</p>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">NAWAB</li>
+    <li class="list-group-item">Delicious food</li>
+    <li class="list-group-item">
+	  Tehzeeb</li>
+  </ul>
+  <div class="card-body" >
+    <a href="https://www.goibibo.com/flights/" class="card-link" style="color:crimson;">Flight Book</a>
+    <a href="https://www.makemytrip.com/railways/?cmp=SEM|D|Rail|G|Brand|Rail_Brand_Exact|Train|ETA|Regular|325113663526&s_kwcid=AL!1631!3!325113663526!e!!g!!make.my%20trip%20train" class="card-link" style="color:crimson;">Train book</a>
+  </div>
+</div>
+		
+		<a href="https://en.wikipedia.org/wiki/Jaipur">	<button style="margin-left: 50%;height: 40px;width:30%; margin-top: -30%;color:crimson;">MORE ABOUT JAIPUR</button></a>
+
+</div>
+
+
+		<footer id="foot">
+			
+		
+			   <div class="footer-column">
+                <ul>
+                    <li><b>LEARN</b></li>
+                    <li><a href="https://www.geeksforgeeks.org/fundamentals-of-algorithms/">Algorithms</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/data-structures/">Data Structures</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/category/program-output/">Languages</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/articles-on-computer-science-subjects-gq/">CS Subjects</a></li>
+                    <li><a href="https://www.youtube.com/geeksforgeeksvideos/">Video Tutorials</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-column">
+                <ul>
+                    <li><b>PRACTICE</b></li>
+                    <li><a href="https://practice.geeksforgeeks.org/courses/">Courses</a></li>
+                    <li><a href="https://practice.geeksforgeeks.org/company-tags/">Company-wise</a></li>
+                    <li><a href="https://practice.geeksforgeeks.org/topic-tags/">Topic-wise</a></li>
+                    <li><a href="https://practice.geeksforgeeks.org/faq.php">How to begin?</a></li>
+                </ul>
+            </div>
+		
+			
+
+			
+
+            <div class="footer-column">
+                <ul>
+                    <li><b>CONTRIBUTE</b>
+                    <li><a href="https://www.geeksforgeeks.org/contribute/">Write an Article</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/write-interview-experience/">Write Interview Experience</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/internship/">Internships</a></li>
+                    <li><a href="https://www.geeksforgeeks.org/how-to-contribute-videos-to-geeksforgeeks/">Videos</a></li>
+                </ul>
+            </div>
+			<br>
+			<br>
+			
+<div class="row">
+
+                  <ul class="icons">
+                    <li background="black"><a href="#" ><i class="fa fa-facebook" ></i></a>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                    <a href="#"><i class="fa fa-google-plus"></i>
+						</a>
+					 </li>
+                   </ul>
+                </div>
+       
+			
+			
+		</footer>
+			
+
+			
+		<script>
+			function alert()
+			{
+				
+				alert(hye);
+				
+			}
+			
+			
+		function abc()
+			{
+				
+				
+				document.querySelector(".front").style.display="block";
+			}
+		function abc2()
+			{
+				
+				
+				document.querySelector(".front").style.display="none";
+			}</script>
+		
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" >
+				</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" 
+		></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+	
+	
+	</body>
+</html>
